6223a8e10d954dc003feba2a0f999ab0a75d549a
venkatesh ramesh <venkatesh.ramesh@nielsen.com>
2017-04-09 00:39:56 +0530

    added missed out city class

---
+++ src/main/java/org/arpit/java2blog/model/City.java
@@ -0,0 +1,68 @@
+package org.arpit.java2blog.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by rameve02 on 08-04-2017.
+ */
+@Entity
+@Table(name="CITY")
+public class City {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name="name")
+    String name;
+
+    @OneToOne(fetch= FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    Country country;
+
+    public City() {
+        super();
+    }
+
+    public City(Integer id, String name, Country country) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", country=" + country +
+                '}';
+    }
+}
